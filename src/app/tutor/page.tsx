25e98a55ade99a4ec521093aab758505f7cd4225
Nunna anil sai <anilsainunna@gmail.com>
2026-06-12 18:02:38 +0530

    feat(modules):Added the tutor and ins_rep modules in the staging to avoid the confusion in the feature.

---
+++ src/app/tutor/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col justify-center items-center gap-6 p-6 overflow-hidden">
+        <p className="text-lg  text-slate-800">Tutor</p>
+        <p className="text-sm text-slate-500">Coming soon...</p>
+      </div>
+    </>
+  );
+}
